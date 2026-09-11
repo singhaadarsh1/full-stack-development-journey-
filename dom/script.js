572d3998797fd01ef95191049c8bbe11c05f06b9
Aadarsh Singh <aadarshsinghsikriwal153@gmail.com>
2026-09-11 16:59:52 +0530

    understand DOM manipulation using state driven frontend (todo app)

---
+++ dom/script.js
@@ -0,0 +1,8 @@
+//const heading = document.querySelector("h1");
+//console.log(heading);
+const todos=document.querySelectorAll("h4")[1];
+console.log(todos);
+const update=document.querySelector("#title");
+update.innerHTML="<span>hello</span>";
+console.log(update);
+
